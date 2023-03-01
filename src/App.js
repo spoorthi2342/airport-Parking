e9476ad57129f9e285e1b763d8922614fb552c4b
spoorthi <spoorthiachar9@gmail.com>
2023-03-02 00:07:26 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import logo from './logo.svg';
+
+import Header from'./pages/Header.js';
+import HomePage from'./pages/HomePage.js';
+import Footer from './pages/Footer.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+    <Footer/>
+    <Header/>
+    <HomePage/></>
+    );
+} 
 
 export default App;
  
---
+++ src/pages/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <footer className="bg-light text-center text-lg-start">
+    <div className="text-center p-3">@ 2023 copyright:
+    <a className="text-dark"href="/">AIRPORT PARKING</a>
+    </div>
+    </footer>
+    </>
+
+  )
+}
+
+export default Footer;
