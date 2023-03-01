90a3f1f8f03e926ef24da2c3705bc5cd988cf667
spoorthi <spoorthiachar9@gmail.com>
2023-03-01 17:56:51 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import HomePage from './pages/HomePage.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <HomePage />
   </React.StrictMode>
 );
 
@@ -15,3 +17,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+<homepage/>
