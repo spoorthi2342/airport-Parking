5203da17b3059af6326cb91959f7abb5d929c2d4
spoorthi <spoorthiachar9@gmail.com>
2023-03-05 16:06:33 +0530

    fifth commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-
 import Header from'./pages/Header.js';
 import HomePage from'./pages/HomePage.js';
 import Footer from './pages/Footer.js';
 import './App.css';
+import React, { useState } from 'react'
+import SearchForm from './pages/SearchForm.js';
+import LoadingBar from 'react-top-loading-bar';
+
 
 function App() {
+const [progress, setProgress] = useState(0);
   return (
     <>
-    <Footer/>
     <Header/>
-    <HomePage/></>
+    <LoadingBar
+          height={3}
+          color='#f11946'
+          progress={progress}
+        />
+    <SearchForm setProgress={setProgress}/>
+    <HomePage/>
+    <Footer/>
+    </>
     );
 } 
 
 export default App;
- 
--- src/pages/Header.js
+++ src/pages/Header.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <><nav className="navbar navbar-expand-lg bg-dark">
+    <>
+      {/* <nav className="navbar navbar-expand-lg bg-dark">
     <div className="container-fluid">
       <a className="navbar-brand" href="/">Airport</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,21 +17,52 @@ const Header = () => {
           <li className="nav-item">
             <a className="nav-link" href="/">Account</a>
           </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul className="dropdown-menu">
-            </ul>
-          </li>
         </ul>
-        <form className="d-flex" role="search">
+        <form className="d-flex mx-10" role="search">
           <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
           <button className="btn btn-outline-success" type="submit">Search</button>
         </form>
       </div>
     </div>
-  </nav></>
+  </nav> */}
+
+      <app-header>
+        <header>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="/"><img src={require("./assets/navigation_logo.png")}
+              alt="AIRPORT PARKING - RESERVATIONS.COM" itemProp="logo" height="40" /></a>
+
+            <div className="col"></div>
+
+            <div className="collapse navbar-collapse flex-row-reverse" id="navbarNavDropdown">
+              <ul className="navbar-nav">
+
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink"
+                    role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    My Account
+                  </a>
+                  <div className="dropdown-menu dropdown-menu-right"
+                    aria-labelledby="navbarDropdownMenuLink">
+
+                    <a className="dropdown-item sign-in" href="/">Sign In</a>
+                    <a className="dropdown-item sign-up" href="/">Register</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </nav>
+            <section id="home_page">
+              <div className="years-of-service">
+                <div className="container">
+                  For 20 years, we’ve helped travelers on their way. With free cancellations & a customer
+                  service team in the US, we are committed to serving you.
+                </div>
+              </div>
+            </section>
+        </header>
+      </app-header>
+    </>
   )
 }
 
